974ba62a14569b981ae61a611f68f36bbce1786f
Rutjake <jarnosanterimusic@gmail.com>
2024-08-27 19:42:21 +0300

    Adding Menu & Menu CSS

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import './App.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Menu from './components/menu/menu';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <p>
-        Jarno
-       </p>
+        <Menu />
       </header>
       <Container>
         <Row>
---
+++ src/components/menu/menu.js
@@ -0,0 +1,24 @@
+import './menu.css';
+
+export default function Menu() {
+    return (
+        <nav className="Navi">
+            <ul>
+                <li>
+                    <a href="#/home">Jarno</a>
+                </li>
+                <li>
+                    <ul>
+                        <li className="menu_items">
+                            <a href="/home">Koti</a>
+                            <a href="/education">Koulutus</a>
+                            <a href="/experience">Työkokemus</a>
+                            <a href="/projects">Projectit</a>
+                            <a href="/contact">Ota Yhteyttä</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+    );
+}

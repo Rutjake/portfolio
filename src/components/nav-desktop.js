ebfdbb83f0afb5b229491d73c0dabe3c923ca806
Rutjake <jarnosanterimusic@gmail.com>
2024-08-30 13:01:14 +0300

    Hamburger Menu Animation

--- src/components/nav-desktop.js
+++ src/components/nav-desktop.js
@@ -5,13 +5,13 @@ import "./nav-desktop.css";
 
 export default function NavDesktop () {
     return (
-        <ul className="hidden lg:flex lg:items-center gap-5 text-sm desktop_links">
-             <h3 className="nav-title">JARNO</h3>
+        <ul className="hidden gap-5 text-sm desktop_links">
+             <h3 className="nav_title">JARNO</h3>
             {routes.map((route, index) => {
                 const { Icon, href, title } = route;
                 return (
                     <li key={index}>
-                        <a href={href} className="flex items-center gap-1 hover:text-neutral-400 transition-all desktop_navList"
+                        <a href={href} className="gap-1 desktop_navList"
                         >
                                <FontAwesomeIcon icon={Icon} className="desktop_icon" />
                             {title}
--- src/components/nav-mobile.js
+++ src/components/nav-mobile.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useClickAway } from "react-use";
 import { useRef } from "react";
 import { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 import { Spin as Hamburger } from "hamburger-react";
 import { routes } from "../routes";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -15,32 +16,50 @@ export default function NavMobile() {
 
     return (
         <div ref={ref} className="lg:hidden">
-            <Hamburger toggled={isOpen} size={20} toggle={setOpen} />
-            {isOpen && (
-                <div className="fixed left-0 shadow-4xl right-0 top-[3.5rem] p-5 pt-0 bg-neutral-950 border-b border-b-white/20">
-                    <ul className="grid gap-2">
-                        {routes.map((route) => {
-                            const { Icon, href, title } = route;
+            <Hamburger label="Valikko" color="rgb(45, 93, 121)" rounded toggled={isOpen} size={20} toggle={setOpen} />
+            <AnimatePresence>
+                {isOpen && (
+                    <motion.div
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        transition={{ duration: 0.2 }}
+                        className="fixed left-0 shadow-4xl right-0 top-[3.5rem] p-5 pt-0 bg-neutral-950 border-b border-b-white/20">
+                        <ul className="grid gap-2">
+                            {routes.map((route, idx) => {
+                                const { Icon, href, title } = route;
 
-                            return (
-                                <li key={title} className="w-full p-[0.08rem] rounded-xl bg-gradient-to-tr from-neutral-800 via-neutral-950 to-neutral-700 mobile_navList"
-                                >
-                                    <a
-                                        onClick={() => setOpen((prev) => !prev)}
-                                        className={
-                                            "flex items-center justify-between w-full p-5 rounded-xl bg-neutral-950"
-                                        }
-                                        href={href}
+                                return (
+                                    <motion.li
+                                        initial={{ opacity: 0, opacity: 0 }}
+                                        animate={{ scale: 1, opacity: 1 }}
+                                        exit={{ opacity: 0 }}
+                                        transition={{ 
+                                            type: "spring",
+                                            stiffness: 260,
+                                            damping: 20,
+                                            delay: 0.1 + idx / 10,
+                                         }}
+                                        key={title}
+                                        className="w-full p-[0.08rem] rounded-xl bg-gradient-to-tr from-neutral-800 via-neutral-950 to-neutral-700 mobile_navList"
                                     >
-                                        <span className="flex gap-1 text-lg">{title}</span>
-                                        <FontAwesomeIcon icon={Icon} className='text-xl'/>
-                                    </a>
-                                </li>
-                            );
-                        })}
-                    </ul>
-                </div>
-            )}
+                                        <a
+                                            onClick={() => setOpen((prev) => !prev)}
+                                            className={
+                                                "flex items-center justify-between w-full p-5 rounded-xl bg-neutral-950"
+                                            }
+                                            href={href}
+                                        >
+                                            <span className="flex gap-1 text-lg">{title}</span>
+                                            <FontAwesomeIcon icon={Icon} className='text-xl' />
+                                        </a>
+                                    </motion.li>
+                                );
+                            })}
+                        </ul>
+                    </motion.div>
+                )}
+            </AnimatePresence>
         </div>
     );
 };

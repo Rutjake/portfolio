5925b22b2f3b5ea749be85135b7922cad96cfc05
Rutjake <jarnosanterimusic@gmail.com>
2024-08-30 11:56:44 +0300

    Added Hamburger menu

--- src/App.js
+++ src/App.js
@@ -2,31 +2,37 @@ import './App.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-/*import Menu from './components/menu/menu';*/
+import MediaQuery from 'react-responsive';
 import MainPage from './Pages/main_page';
 import NavDesktop from './components/nav-desktop';
+import NavMobile from './components/nav-mobile';
 
 function App() {
   return (
-      <Container fluid className='App'>
-          <Row>
-            <Col>
-            <nav className="navigation">
-            <NavDesktop />
-            </nav>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-            <h3 className='underText'>Ethical Hacker & Developer</h3>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <MainPage />
-            </Col>
-          </Row>  
-      </Container>
+    <Container fluid className='App'>
+      <Row>
+        <Col>
+          <nav className="navigation">
+            <MediaQuery query="(min-width: 600px)">
+              <NavDesktop />
+            </MediaQuery>
+            <MediaQuery query="(max-width: 600px)">
+              <NavMobile />
+            </MediaQuery>
+          </nav>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <h3 className='underText'>Ethical Hacker & Developer</h3>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <MainPage />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
--- src/components/nav-mobile.js
+++ src/components/nav-mobile.js
@@ -1,7 +1,46 @@
-import useClickAway from "react-use";
-import useRef from "react";
-import useState from "react";
-import {Squash as Hamburger} from "hamburger-react";
-import {AnimatePresence, motion} from "framer-motion";
-import routes from "../routes";
+import React from 'react';
+import { useClickAway } from "react-use";
+import { useRef } from "react";
+import { useState } from "react";
+import { Spin as Hamburger } from "hamburger-react";
+import { routes } from "../routes";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import "./nav-mobile.css";
 
+export default function NavMobile() {
+    const [isOpen, setOpen] = useState(false);
+    const ref = useRef(null);
+
+    useClickAway(ref, () => setOpen(false));
+
+    return (
+        <div ref={ref} className="lg:hidden">
+            <Hamburger toggled={isOpen} size={20} toggle={setOpen} />
+            {isOpen && (
+                <div className="fixed left-0 shadow-4xl right-0 top-[3.5rem] p-5 pt-0 bg-neutral-950 border-b border-b-white/20">
+                    <ul className="grid gap-2">
+                        {routes.map((route) => {
+                            const { Icon, href, title } = route;
+
+                            return (
+                                <li key={title} className="w-full p-[0.08rem] rounded-xl bg-gradient-to-tr from-neutral-800 via-neutral-950 to-neutral-700 mobile_navList"
+                                >
+                                    <a
+                                        onClick={() => setOpen((prev) => !prev)}
+                                        className={
+                                            "flex items-center justify-between w-full p-5 rounded-xl bg-neutral-950"
+                                        }
+                                        href={href}
+                                    >
+                                        <span className="flex gap-1 text-lg">{title}</span>
+                                        <FontAwesomeIcon icon={Icon} className='text-xl'/>
+                                    </a>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </div>
+            )}
+        </div>
+    );
+};

b8003072872d3322de2d5ec111682b40b42d41df
Rutjake <jarnosanterimusic@gmail.com>
2024-10-02 19:25:25 +0300

    Projects page updated

--- src/Pages/projects.js
+++ src/Pages/projects.js
@@ -7,6 +7,9 @@ export default function Projects() {
     return (
         <div className='mainText'>
             <h3 className='subTitle'>Projektit</h3>
+            <p>Lisää projekteja löydät <a href="https://github.com/rutjake" target="_blank">Githubista.</a></p>
+            <p>Tällä sivulla kerrotaan myös muista kuin julkisista projekteista.</p>
+
             <Cards />
         </div>
     );

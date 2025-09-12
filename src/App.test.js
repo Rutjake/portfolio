39e240369b110dce4bdc91d9b0acb8ecea554a96
Rutjake <jarnosanterimusic@gmail.com>
2025-09-12 17:05:20 +0300

    Moved From React To Next.js

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders underText', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Ethical Hacker & Developer/i);
-  expect(linkElement).toBeInTheDocument();
-});
